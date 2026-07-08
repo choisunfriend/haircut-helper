627858673f748ffce7e6027c703862691deea1ca
choisunfriend <choisunfriend@gmail.com>
2026-07-08 11:37:09 +0900

    Delete vision-orientation.js

--- vision-orientation.js
+++
@@ -1,7 +0,0 @@
-let __visionModulePromise = null;
-function loadVisionModule(){
-  if(!__visionModulePromise){
-    __visionModulePromise = import('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.9');
-  }
-  return __visionModulePromise;
-}
